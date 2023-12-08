c76d917e6dc11344e00815900791103bae820f62
Misanth420 <misannthh@protonmail.com>
2023-12-08 11:39:52 +0100

    sourced hypercerts sdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,43 @@
-import { ClaimPreviewGrid, Product } from './components/claim-preview';
-import Footer from './components/footer';
-import Hero from './components/hero';
-import { Navigation } from './components/navigation';
-import { HypercertFetcher, HypercertFetcherProps } from './components/hypercert-fetcher';
-import { useState, useEffect } from 'react';
-import { HypercertClient } from '@hypercerts-org/sdk';
-import { DEFAULT_CHAIN_ID } from '@/lib/config';
-
-
+"use client";
+// import { ClaimPreviewGrid, Product } from "./components/claim-preview";
+// import Footer from "./components/footer";
+// import Hero from "./components/hero";
+// import { Navigation } from "./components/navigation";
+// import {
+//   HypercertFetcherProps,
+// } from "./components/hypercert-fetcher";
+import { useState, useEffect } from "react";
+import { HypercertClient } from "@hypercerts-org/sdk";
+import { DEFAULT_CHAIN_ID } from "@/lib/config";
 
 export default function Home() {
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const client = new HypercertClient({
-      chain: { id: DEFAULT_CHAIN_ID }
-    })
-  })
-  return (
-    <>
-      <header>
-        <Navigation />
-      </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Hero />
-        {/*<ClaimPreviewGrid products={products} /> */}
-        <HypercertFetcher
-          byClaimId="0x822f17a9a5eecfd66dbaff7946a8071c265d1d07-187155301806516154904856034087472516300800"
-          byMetadataUri=""
-          useQueryString={true}
-          ignoreLoading={false}
-          loading={<p>Loading...</p>}
-        >
-        </HypercertFetcher>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </>
-  )
+      chain: { id: DEFAULT_CHAIN_ID },
+    });
+  }, []);
+  return null
+  // return (
+  //   <>
+  //     <header>
+  //       <Navigation />
+  //     </header>
+  //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //       <Hero />
+  //       {/*<ClaimPreviewGrid products={products} /> */}
+  //       <HypercertFetcher
+  //         byClaimId="0x822f17a9a5eecfd66dbaff7946a8071c265d1d07-187155301806516154904856034087472516300800"
+  //         byMetadataUri=""
+  //         useQueryString={true}
+  //         ignoreLoading={false}
+  //         loading={<p>Loading...</p>}
+  //       ></HypercertFetcher>
+  //     </main>
+  //     <footer>
+  //       <Footer />
+  //     </footer>
+  //   </>
+  // );
 }
